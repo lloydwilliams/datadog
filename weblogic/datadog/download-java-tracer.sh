1e5ac2df5a4b1a805694d6ab589e9d4c05f1b320
lloydwilliams <lloyd.williams@datadoghq.com>
2021-10-22 10:20:04 -0400

    added wget --no-check-certificate
    added wget --no-check-certificate

--- weblogic/datadog/download-java-tracer.sh
+++ weblogic/datadog/download-java-tracer.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
+#wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
+wget --no-check-certificate -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
