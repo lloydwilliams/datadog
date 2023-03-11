4b0f8a91a269ab3a5e6c8164c4c2c771f2475b6f
lloydwilliams <lloyd.williams@datadoghq.com>
2023-03-10 20:03:11 -0500

    moved
    moved

--- observability-pipelines/examples/gates-app/start-observability-pipelines-worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# OP = https://www.datadoghq.com/product/observability-pipelines/
-
-# since this observability pipeline sends logs to Datadog you will need to configure an environment variable with your DD_API_KEY
-export DD_API_KEY=${DD_API_KEY}
-#export DD_OP_CONFIG_KEY=${DD_OP_CONFIG_KEY}
-export DD_OP_CONFIG_KEY=9702ba64-bee4-11ed-b826-da7ad0900002
-
-vector --config ./observability-pipelines-worker.yaml
--- observability-pipelines/examples/gates-app/start-vector.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# OP = https://www.datadoghq.com/product/observability-pipelines/
-
-vector --config ./observability-pipelines-worker.yaml
